7cfcbf474e7e6527cdd52e1b45c44faa98b9709a
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2020-10-07 10:56:47 +0200

    add support for --token

--- tfe_run_wait/cli.py
+++ tfe_run_wait/cli.py
@@ -8,13 +8,13 @@ from typing import Iterable, Optional
 
 import requests
 
-token = os.getenv("TFE_TOKEN")
+_tfe_api_token = os.getenv("TFE_API_TOKEN")
 
 Run = namedtuple("Run", ["run", "ingress_attributes"])
 
 
-def get(path: str, params: dict = {}) -> Optional[dict]:
-    hdrs = {"Authorization": f"Bearer {token}"}
+def _get(path: str, params: dict = {}) -> Optional[dict]:
+    hdrs = {"Authorization": f"Bearer {_tfe_api_token}"}
 
     if path.startswith("/api/") or path.startswith("api/"):
         url = f'https://app.terraform.io/{path.lstrip("/")}'
@@ -30,9 +30,9 @@ def get(path: str, params: dict = {}) -> Optional[dict]:
         raise Exception(f"failed to get {path}, status code {r.status_code}, {r.text}")
 
 
-def list(path: str, headers: dict = {}, params: dict = {}) -> Iterable[dict]:
+def _list(path: str, headers: dict = {}, params: dict = {}) -> Iterable[dict]:
     prms = {"page[size]": 15}
-    hdrs = {"Authorization": f"Bearer {token}"}
+    hdrs = {"Authorization": f"Bearer {_tfe_api_token}"}
     if headers:
         hdrs.update(headers)
     if params:
@@ -60,7 +60,7 @@ def list(path: str, headers: dict = {}, params: dict = {}) -> Iterable[dict]:
 
 
 def find_run_for_commit(workspace: dict, url: str, commit_sha: str) -> Optional[Run]:
-    for run in list(f'workspaces/{workspace["id"]}/runs'):
+    for run in _list(f'workspaces/{workspace["id"]}/runs'):
         configuration_version_id = (
             run.get("relationships", {})
             .get("configuration-version", {})
@@ -68,7 +68,7 @@ def find_run_for_commit(workspace: dict, url: str, commit_sha: str) -> Optional[
             .get("id")
         )
         if configuration_version_id:
-            ingress_attributes = get(
+            ingress_attributes = _get(
                 f"configuration-versions/{configuration_version_id}/ingress-attributes"
             )
             if ingress_attributes:
@@ -86,14 +86,14 @@ def get_apply(run: dict) -> Optional[dict]:
     apply_link = (
         run.get("relationships", {}).get("apply", {}).get("links", {}).get("related")
     )
-    return get(apply_link)
+    return _get(apply_link)
 
 
 def get_plan(run: dict) -> Optional[dict]:
     plan_id = (
         run.get("relationships", {}).get("plan", {}).get("links", {}).get("related")
     )
-    return get(plan_id)
+    return _get(plan_id)
 
 
 def wait_until(
@@ -102,7 +102,7 @@ def wait_until(
     clone_url: str,
     commit_sha: str,
     maximum_wait_time_in_seconds: int,
-    verbose:bool = False,
+    verbose: bool = False,
 ):
     workspace_name = workspace["attributes"]["name"]
     now = start_time = time()
@@ -110,7 +110,7 @@ def wait_until(
         run = find_run_for_commit(workspace, clone_url, commit_sha)
         if run:
             if verbose:
-                logging.info("%s\n", json.dumps(run,indent=2))
+                logging.info("%s\n", json.dumps(run, indent=2))
 
             run_id = run.run["id"]
             status = run.run.get("attributes").get("status")
@@ -163,10 +163,30 @@ def wait_until(
     )
 
 
+class EnvDefault(argparse.Action):
+    def __init__(self, envvar, required=True, default=None, **kwargs):
+        if not default and envvar:
+            if envvar in os.environ:
+                default = os.environ[envvar]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="wait for TFE run to reach specified state."
     )
+    parser.add_argument(
+        "--token",
+        action=EnvDefault,
+        envvar="TFE_API_TOKEN",
+        help="Terraform Enterprise access token, default from TFE_API_TOKEN",
+    )
+
     parser.add_argument("--organization", required=True, help="of the workspace")
     parser.add_argument("--workspace", required=True, help="to inspect runs for")
     parser.add_argument(
@@ -197,11 +217,14 @@ def main():
     )
     args = parser.parse_args()
 
-    org = get(f"organizations/{args.organization}")
+    global _tfe_api_token
+    _tfe_api_token = args.token
+
+    org = _get(f"organizations/{args.organization}")
     if not org:
         parser.error(f"unknown organization {args.organization}.")
 
-    workspace = get(f"organizations/{args.organization}/workspaces/{args.workspace}")
+    workspace = _get(f"organizations/{args.organization}/workspaces/{args.workspace}")
     if not workspace:
         parser.error(
             f"workspace {args.workspace} is unknown in organization {args.organization}."
@@ -214,7 +237,7 @@ def main():
             args.clone_url,
             args.commit_sha,
             args.maximum_wait_time * 60,
-            args.verbose
+            args.verbose,
         )
     )
 
