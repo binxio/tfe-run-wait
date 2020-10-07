6eb866f368dc6ccbb960660ec687c02d003f6fe4
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2020-10-07 14:32:29 +0200

    bumped  to release 0.3.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = ['requests']
 
 setup(
     name='tfe-run-wait',
-    version='0.3.2',
+    version='0.3.3',
     url='https://github.com/binxio/tfe-run-wait',
     license='BSD',
     author='Mark van Holsteijn',
