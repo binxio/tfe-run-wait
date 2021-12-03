063f3ec687719efd6c3ea0f51c421ee9dc022316
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2021-12-03 18:25:14 +0100

    bumped  to release 0.6.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name='tfe-run-wait',
-    version='0.5.1',
+    version='0.6.0',
     url='https://github.com/binxio/tfe-run-wait',
     license='BSD',
     author='Mark van Holsteijn',
